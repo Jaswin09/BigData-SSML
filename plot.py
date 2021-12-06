0cfe6cc72609923e07d74cf1a7776fd4dfa3b075
Jaaswin D Kotian <56599118+Jaswin09@users.noreply.github.com>
2021-12-06 22:29:00 +0530

    Add files via upload

--- plot.py
+++ plot.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
-
+import json
+import numpy as np
 nb=[]
 pac=[]
 perc=[]
-with open('nb','r') as f:
-	for i in f:
-		if i:
-			nb.append(float(i.strip()))
+with open('nb200','r') as f:
+	c=f.readlines()
+	for i in range(0,len(c),2):
+		nb.append(float(c[i].strip()))	
+	l1=json.loads(c[-1])
+			
+with open('pac200','r') as f:
+	c=f.readlines()
+	for i in range(0,len(c),2):
+		pac.append(float(c[i].strip()))
+	l2=json.loads(c[-1])
 				
-with open('pac','r') as f:
-	for i in f:
-		if i:
-			pac.append(float(i.strip()))
-				
-with open('perc','r') as f:
-	for i in f:
-		if i:
+with open('perc200','r') as f:
+	c=f.readlines()
+	for i in range(0,len(c),2):
+		perc.append(float(c[i].strip()))
+	l3=json.loads(c[-1])
 	
-			perc.append(float(i.strip()))
-
 def lineplot():
 	#print(nb,pac,perc)
 	batch=[i for i in range(len(nb))]
@@ -61,6 +64,49 @@ def barplot():
 	plt.ylabel("Accuracy")
 	plt.title("Classifier Comparison")
 	plt.show()
+
+def total_metrics():
+	s1=0
+	s2=0
+	s3=0
+	for i in range(len(l1)):
+		for j in range(len(l1[i])):
+			s1+=l1[i][j]
+			s2+=l2[i][j]
+			s3+=l3[i][j]
+	X=['MUltinomialNB','PassiveAggressiveClassifier','Perceptron']
+	
+	acc1=(l1[0][0]+l1[1][1])/s1
+	acc2=(l2[0][0]+l2[1][1])/s2
+	acc3=(l3[0][0]+l3[1][1])/s3
+	
+	rec1=l1[0][0]/(l1[0][0]+l1[0][1])
+	rec2=l2[0][0]/(l2[0][0]+l2[0][1])
+	rec3=l3[0][0]/(l3[0][0]+l3[0][1])
+	
+	pre1=l1[0][0]/(l1[0][0]+l1[1][0])
+	pre2=l2[0][0]/(l2[0][0]+l2[1][0])
+	pre3=l3[0][0]/(l3[0][0]+l3[1][0])
+	
+	acc=[acc1,acc2,acc3]
+	rec=[rec1,rec2,rec3]
+	pre=[pre1,pre2,pre3]
+	
+	X_axis=np.arange(len(acc))
+	plt.bar(X_axis,acc,0.1,label="accuracy")
+	plt.bar(X_axis+0.1,rec,0.1,label="recall")
+	plt.bar(X_axis-0.1,pre,0.1,label="precision")
+	
+	plt.xticks(X_axis,X)
+	plt.xlabel("Classifiers")
+	plt.ylabel("metrics")
+	plt.title("Classifier Comparison")
+	plt.legend()
+	plt.show()
+
+	print(acc1,acc2,acc3)
+	
 	
 lineplot()
 barplot()
+total_metrics()
--- test.py
+++ test.py
@@ -10,13 +10,17 @@ from pyspark.sql.types import StringType
 from models import *
 from model_train import *
 from feature_extract import *
+from sklearn.metrics import confusion_matrix
 sc = SparkContext.getOrCreate()
-#sc.setLogLevel("OFF")
+sc.setLogLevel("OFF")
 ssc = StreamingContext(sc, 1)
 spark=SparkSession(sc)
 data = ssc.socketTextStream("localhost", 6100)
 print(data)
 
+confnb=[[0,0],[0,0]]
+confperc=[[0,0],[0,0]]
+confpac=[[0,0],[0,0]]
 try:
 	def stream_read(rdd):
 		df=spark.read.json(rdd)
@@ -24,33 +28,48 @@ try:
 			df = preprocess_transform(df)
 			x,y=features(df)
 			
-			with open('nb_model2','rb') as f1:
+			with open('nb_model1','rb') as f1:
 				lr1 = pickle.load(f1)
 			y_pred1 = lr1.predict(x)
 			acc1 = accuracy_score(y,y_pred1)
-			print('NB :',acc1)
+			conf1=confusion_matrix(y,y_pred1)
+			for i in range(len(conf1)):
+				for j in range(len(conf1[i])):
+					confnb[i][j]+=conf1[i][j]
+			print('NB :',acc1,confnb)
 			
 			
-			with open('perc_model2','rb') as f2:
+			with open('perc_model1','rb') as f2:
 				lr2 = pickle.load(f2)
 			y_pred2 = lr2.predict(x)
 			acc2 = accuracy_score(y,y_pred2)
-			print('PERC : ',acc2)
+			conf2=confusion_matrix(y,y_pred2)
+			for i in range(len(conf2)):
+				for j in range(len(conf2[i])):
+					confperc[i][j]+=conf2[i][j]
+			print('PERC :',acc2,confperc)
 			
 			
-			with open('pac_model2','rb') as f3:
+			with open('pac_model1','rb') as f3:
 				lr3 = pickle.load(f3)
 			y_pred3 = lr3.predict(x)
 			acc3 = accuracy_score(y,y_pred3)
-			print('PAC :',acc3)
+			conf3=confusion_matrix(y,y_pred3)
+			for i in range(len(conf3)):
+				for j in range(len(conf3[i])):
+					confpac[i][j]+=conf3[i][j]
+			print('PAC :',acc3,confpac)
 			
 			
-			with open('nb','a') as f4:
+			with open('nb200','a') as f4:
 				f4.write(str(acc1)+'\n')
-			with open('perc','a') as f5:
+				f4.write(str(confnb)+'\n')
+			with open('perc200','a') as f5:
 				f5.write(str(acc2)+'\n')
-			with open('pac','a') as f6:
+				f5.write(str(confperc)+'\n')
+			with open('pac200','a') as f6:
 				f6.write(str(acc3)+'\n')
+				f6.write(str(confpac)+'\n')
 		
 		
 except Exception as e:
